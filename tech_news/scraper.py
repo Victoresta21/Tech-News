9c6b524983b97d244a459ac097e93e494bcdf011
Victor Emmanuel <victorpassos2012@gmail.com>
2022-08-30 17:44:55 -0300

    Requisito 4 feito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,7 +38,29 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    select = parsel.Selector(text=html_content)
+    url = select.css("link[rel='canonical']::attr(href)").get()
+    title = select.css('.entry-title::text').get().strip()
+    timestamp = select.css('.meta-date::text').get()
+    writer = select.css('.author a::text').get()
+    comments_count = len(select.css('#comment').getall())
+    summary = select.xpath("string(.//div[@class='entry-content']/p)")
+    summary_new = summary.get().strip()
+    tags = select.css("a[rel='tag']::text").getall()
+    category = select.css('.label::text').get()
+
+    dict_news = {
+      'url': url,
+      'title': title,
+      'timestamp': timestamp,
+      'writer': writer,
+      'comments_count': comments_count,
+      'summary': summary_new,
+      'tags': tags,
+      'category': category,
+    }
+
+    return dict_news
 
 
 # Requisito 5
